5944bad2324bcb9ea49d4d95fb90d3293051820e
Colin <colinwitkamp@gmail.com>
2023-04-18 10:08:22 -0700

    server: server log

--- server.js
+++ server.js
@@ -8,4 +8,6 @@ app.use(express.static('resources'));
 //Serves all the request which includes /images in the url from Images folder
 app.use('/api', express.static(__dirname + '/resources'));
 
-app.listen(5000);
+app.listen(5000, () => {
+    console.log('Server is running at 5000')
+});
